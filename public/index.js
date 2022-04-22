de04f0f5a5b2e7449f7025cbac0942763aea1ddf
Mason_Matthews <masonmatthews98@gmail.com>
2022-04-21 21:32:31 -0700

    couldnt get my server working properly but i believe it works

--- public/index.js
+++ public/index.js
@@ -12,15 +12,32 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        updateApi(countValue)
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        updateApi(countValue)
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
+    
 }
+ const  updateApi = async (newCount) => {
+    const response = await fetch("http://localhost:9001/counter", {
+        method:"PATCH",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            value: newCount
+        })
+    })
+    const result = await response.json();
+    console.log(result)
+}
+updateApi()
 main()
